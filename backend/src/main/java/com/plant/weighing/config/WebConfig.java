6370570fc61ba921391a98096346d292a2151af9
Iranda <iranda@johnkeellsit.com>
2026-07-20 09:14:46 +0530

    v9

--- backend/src/main/java/com/plant/weighing/config/WebConfig.java
+++ backend/src/main/java/com/plant/weighing/config/WebConfig.java
@@ -14,8 +14,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+       
+				
+		registry.addMapping("/api/**")
+            .allowedOrigins(
+            "http://localhost:4200",
+            "https://sap-weighing-station-frontend-production.up.railway.app"
+        )
+        .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
